b4149a6c885914a2acb44f5bce83fda55527dfe4
Hugo Guevara <hugo.v.guevara@hotmail.com>
2021-09-17 11:30:03 -0300

    Actualizacion Node.h

--- Node.h
+++ Node.h
@@ -7,22 +7,40 @@ namespace TypeNode{
 
 	struct Node;
 
+	//Pre: -
+	//Pos: Crea y devuelve un @Node* vacio
 	Node* CreateEmpty();
 
+	//Pre: @card es una Card* valida no vacia
+	//Pos: Crea y devuelve un @Node* con @card como contenido
 	Node* Create(Card* card);
 
+	//Pre: @node y @newPrevious son Node* validos no vacios
+	//Pos: asigna en @Node como @previous a @newPrevious
 	void SetPrevious(Node* node, Node* newPrevious);
 
+	//Pre: @node es un Node* valido no vacio
+	//Pos: devuelve lo contenido en @node como @card
 	Card* GetCard(Node* node);
 
+	//Pre: @node es un Node* valido no vacio
+	//Pos: devuelve lo contenido en @node como @previous
 	Node* GetPrevious(Node* node);
 
+	//Pre: @node es un Node* valido no vacio
+	//Pos: devuelve lo contenido en @node en @card como @suit
 	char* GetSuit(Node* node);
 
+	//Pre: @node es un Node* valido no vacio
+	//Pos: devuelve lo contenido en @node en @card como @values
 	int GetValues(Node* node);
 
+	//Pre: @node es un Node* valido no vacio
+	//Pos: muestra por pantalla el contenido de @node
 	void View(Node* node);
 
+	//Pre: @node es un Node*
+	//Pos: libera todos los recursos utilizados por @node
 	void Destroy(Node* node);
 
 }
